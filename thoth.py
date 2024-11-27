bc8c36f0a16b2d62b0c6dc4bbbffd2fe85cc7fbf
Nat Raway <nathanael.raway@gmail.com>
2024-11-27 10:02:49 -0600

    added fire cli

---
+++ thoth.py
@@ -0,0 +1,15 @@
+import fire
+from ingest_tabs import ingest_file, open_tab_group
+
+
+class Client:
+
+    def ingest(self, file_path):
+        return ingest_file(file_path)
+
+    def open(self, group_name):
+        return open_tab_group(group_name)
+
+
+if __name__ == "__main__":
+    fire.Fire(Client)
