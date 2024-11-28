57fd1d778fe432630ccd9c3ac3b0f9ffc43caa74
Nat Raway <nathanael.raway@gmail.com>
2024-11-27 18:36:23 -0600

    black friday query relative

--- utils.sql
+++ utils.sql
@@ -1,12 +1,16 @@
 SELECT pg_size_pretty(pg_total_relation_size('email')) AS total_size;
 
 -- Black Friday sales 2024
-select distinct on (sender_domain)
-    concat('https://mail.google.com/mail/u/0/#inbox/', message_id),
-    *,
-    substring(sender from '@(.*)$') AS sender_domain
-from email
-where concat(subject, body) ilike '%black friday%'
-and date > '2023-11-01'
-and date < '2024-11-01'
-order by sender_domain, date desc;
+WITH target_emails AS (
+    SELECT *
+    FROM email
+    WHERE concat(subject, body) ILIKE '%black friday%'
+      AND date > current_date - interval '1 year' -- Adjust dynamically
+      AND date < current_date
+)
+SELECT DISTINCT ON (sender_domain)
+       concat('https://mail.google.com/mail/u/0/#inbox/', message_id) AS gmail_link,
+       *,
+       substring(sender FROM '@(.*)$') AS sender_domain
+FROM target_emails
+ORDER BY sender_domain, date DESC;
